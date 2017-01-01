aeb6f5222525d89bc12d1694f41c60bb8e6ec346
davidmscott <dscottm@gmail.com>
2017-01-01 00:03:02 -0700

    Add API endpoints for foods and dates

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const {ObjectID} = require('mongodb');
 // const moment = require('moment') after installing moment for date formatting
 
 const {mongoose} = require('./db/mongoose.js');
@@ -15,7 +16,7 @@ app.use(express.static(__dirname));
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -25,14 +26,14 @@ var apiKey = '9c762c838016043633065dcee0261687';
 var apiId = 'ab3d9cfa';
 var baseUrl = `https://api.edamam.com/api/nutrition-data?app_id=${apiId}&app_key=${apiKey}&ingr=`;
 
-app.get('/nutrients', function(req, res) {
+app.get('/nutrients', (req, res) => {
 	// res.setHeader('Access-Control-Allow-Origin', '*');
 	// res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 	// res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype');
 	// res.setHeader('Access-Control-Allow-Credentials', true);
 	var term = req._parsedUrl.query;
 	var url = `${baseUrl}${term}`;
-	request(url, function(error, response, body) {
+	request(url, (error, response, body) => {
 		console.log('request', typeof response);
 		var foodItem = new FoodItem({
 			user: 'Dave',
@@ -51,6 +52,50 @@ app.get('/nutrients', function(req, res) {
 	});
 });
 
-app.listen(port, function() {
+app.get('/foods', (req, res) => {
+	console.log(req);
+	FoodItem.find({date: req.body}).then((foodList) => {
+		res.send({foodList});
+	}, (error) => {
+		res.status(400).send(error);
+	})
+});
+
+app.get('/dates', (req, res) => {
+	console.log(req);
+	FoodItem.find().then((foodList) => {
+		var dateList = foodList.map((foodItem) => {
+			return foodItem.date;
+		});
+		dateList = dateList.filter((date, index) => {
+			return dateList.indexOf(date) === index;
+		});
+		res.send({dateList});
+	}, (error) => {
+		res.status(400).send(error);
+	});
+});
+
+app.delete('/food', (req, res) => {
+	FoodItem.findByIdAndRemove(reg.body.id).then((food) => {
+		if (!food) {
+			return res.status(404).send();
+		}
+
+		res.send(food);
+	}).catch((error) => {
+		res.status(400).send(error);
+	});
+});
+
+app.delete('/date', (req, res) => {
+	FoodItem.remove({date: req.body}).then((result) => {
+		res.send(result.result);
+	}).catch((error) => {
+		res.status(400).send(error);
+	});
+});
+
+app.listen(port, () => {
 	console.log('Server running on port', port);
 });
